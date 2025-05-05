e00bbef2ee82347365b18aff1bcb6a38480816dc
Nicolas Rios <dnicolas.rios98@gmail.com>
2025-05-05 20:53:50 +0200

    test: enhance ProductsPage tests with MockWebServer and add mock product data

--- src/pages/__tests__/ProductsPage.spec.tsx
+++ src/pages/__tests__/ProductsPage.spec.tsx
@@ -1,18 +1,36 @@
 import { render, RenderResult, screen } from "@testing-library/react";
-import { test } from "vitest";
-import { ProductsPage } from "../ProductsPage";
 import { ReactNode } from "react";
+import { afterAll, afterEach, beforeAll, describe, test } from "vitest";
 import { AppProvider } from "../../context/AppProvider";
+import { ProductsPage } from "../ProductsPage";
+import { MockWebServer } from "../../tests/MockWebServer";
+import productResponseJSON from "../__tests__/data/productsResponse.json";
 
-test("should display title", async () => {
-    renderComponent(<ProductsPage />);
-    await screen.findByRole("heading", { name: "Product price updater" });
+const mockWebServer = new MockWebServer();
+
+describe("<ProductsPage />", () => {
+    beforeAll(() => mockWebServer.start());
+    afterEach(() => mockWebServer.resetHandlers());
+    afterAll(() => mockWebServer.close());
+
+    test("should display title", async () => {
+        givenProducts();
+        renderComponent(<ProductsPage />);
+        await screen.findByRole("heading", { name: "Product price updater" });
+    });
 });
 
+function givenProducts() {
+    mockWebServer.addRequestHandlers([
+        {
+            method: "get",
+            endpoint: "https://fakestoreapi.com/products",
+            httpStatusCode: 200,
+            response: productResponseJSON,
+        },
+    ]);
+}
+
 function renderComponent(component: ReactNode): RenderResult {
-    return render(
-        <AppProvider>
-            {component}
-        </AppProvider>
-    )
+    return render(<AppProvider>{component}</AppProvider>);
 }
